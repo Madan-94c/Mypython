185e3512cbb030d9b237890419960bdf53d15fcc
Madan-94c <madancmadanc498@gmail.com>
2025-11-11 16:25:40 +0530

    Create break statement.py

---
+++ break statement.py
@@ -0,0 +1,5 @@
+#Write a Python program using a for loop and break statement to print numbers from 1 to 20, but stop when the number reaches 13.
+for num in range(1, 21):
+  if num == 13:
+    break
+  print(num)
