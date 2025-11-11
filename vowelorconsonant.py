dfb7580a3001f59f308679f80e1a5c6dad14bf81
Madan-94c <madancmadanc498@gmail.com>
2025-11-11 14:32:32 +0530

    Create vowelorconsonant.py

---
+++ vowelorconsonant.py
@@ -0,0 +1,6 @@
+#Write a Python program to check whether a given character is a vowel or a consonant.
+c = (input("Enter the character:"))
+if c in "aeiou":
+  print("vowel")
+else:
+  print("consonant")
