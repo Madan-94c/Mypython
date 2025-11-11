5beceb14d3a8d03572d66b96b1971b3fc8e75850
Madan-94c <madancmadanc498@gmail.com>
2025-11-11 14:30:50 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,6 @@
+#Write a Python program to check whether a person is eligible to vote or not (age ≥ 18).
+age = int(input("Enter the age:"))
+if age>=18:
+  print("The person is eligible to vote")
+else:
+  print("The person is not eligible to vote")
