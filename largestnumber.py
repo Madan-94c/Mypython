fc1ff6c123ebc7837426e36434140000c467c440
Madan-94c <madancmadanc498@gmail.com>
2025-11-11 12:23:17 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,7 @@
+#Write a Python program to find the largest number between two numbers.
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+if num1>num2:
+  print("The largest number",num1)
+else:
+  print("No largest number")
