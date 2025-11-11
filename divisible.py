b5d4286dba55bfe96bb084df894de4e4f4712a09
Madan-94c <madancmadanc498@gmail.com>
2025-11-11 14:33:43 +0530

    Create divisible.py

---
+++ divisible.py
@@ -0,0 +1,6 @@
+#Write a Python program to check if a number is divisible by both 3 and 5.
+num = int(input("Enter the number:"))
+if (num%3==0) & (num%5==0):
+  print("Divisible by both 3 and 5")
+else:
+  print("Not divisible by both 3 and 5")
