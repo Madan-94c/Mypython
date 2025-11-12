6a0556f3cd77e2b8302c408995769fb26928c20f
Madan-94c <madancmadanc498@gmail.com>
2025-11-12 10:17:26 +0530

    Create formated string example.py

---
+++ formated string example.py
@@ -0,0 +1,6 @@
+#formatted string.
+name = "Madan"
+age = 18
+college = "Shri devi institute of Engineering"
+place = "Tumkur"
+print(f"My name is: {name} and my age is: {age} i am studing in: {college} it is in: {place}")
