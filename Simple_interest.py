7bfcce4cbd1126d26d258662880a476cec09494f
Madan-94c <madancmadanc498@gmail.com>
2025-11-12 16:09:16 +0530

    Create Simple_interest.py

---
+++ Simple_interest.py
@@ -0,0 +1,5 @@
+#5. Write a function simple_interest() that takes principal, rate, and time as parameters and returns the simple interest.
+
+def simple_interest(principal,rate,time):
+  return (principal*rate*time)/100
+simple_interest(2000,50,15)
