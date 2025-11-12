1163ac6ae4faae644fb02591d15d122dfd8f9de3
Madan-94c <madancmadanc498@gmail.com>
2025-11-12 10:22:26 +0530

    Update formated string example.py

--- formated string example.py
+++ formated string example.py
@@ -3,4 +3,4 @@ name = "Madan"
 age = 18
 college = "Shri devi institute of Engineering"
 place = "Tumkur"
-print(f"My name is: {name} and my age is: {age} i am studing in: {college} it is in: {place}")
+print(f"My name is: {name} and my age is: {age} i am studying in: {college} it is in: {place}")
