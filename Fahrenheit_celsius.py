4173ee14680e2794582b8efb96a8412590cf8fe0
Madan-94c <madancmadanc498@gmail.com>
2025-11-12 16:22:39 +0530

    Create Fahrenheit_celsius.py

---
+++ Fahrenheit_celsius.py
@@ -0,0 +1,5 @@
+7. Write a function fahrenheit_to_celsius() that converts temperature from Fahrenheit to Celsius and returns the result.
+fahrenheit = int(input("Enter the temperature:"))
+def fahrenheit_celsius():
+  return fahrenheit-32*5/9
+fahrenheit_celsius()
