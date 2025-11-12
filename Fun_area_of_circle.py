bc1b34edc6551eabea838e5dfe9ef9cd26abff77
Madan-94c <madancmadanc498@gmail.com>
2025-11-12 16:03:24 +0530

    Create Fun_area_of_circle.py

---
+++ Fun_area_of_circle.py
@@ -0,0 +1,5 @@
+#3. Write a function area_of_circle() that takes radius as a parameter and returns the area of the circle.
+pi=3.14
+def area_of_circle(radius):
+  return pi*(radius**2)
+area_of_circle(4)
