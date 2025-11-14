f5aeb0e18240c5c47f70dc380850af0560d948d3
Madan-94c <madancmadanc498@gmail.com>
2025-11-14 12:31:24 +0530

    Create List_Sum_average.py

---
+++ List_Sum_average.py
@@ -0,0 +1,6 @@
+#Write a Python program to find the sum and average of all elements in a list.
+a =[1,2,3,4,5,]
+total= sum(a)
+average=sum(a)/len(a)
+print("The sum is:" ,total)
+print("The average is:",average)
