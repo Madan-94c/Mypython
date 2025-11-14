266dd6b855779cd8e7bd01997eee46774ed55cfb
Madan-94c <madancmadanc498@gmail.com>
2025-11-14 14:28:44 +0530

    Create appear_list.py

---
+++ appear_list.py
@@ -0,0 +1,6 @@
+#4. Write a Python program to count how many times a particular element appears in a list.
+a = [1,2,2,3,4,5,5,5,5,9]
+
+element_to_count = 5
+count = a.count(element_to_count)
+print(f"The element {element_to_count} appears {count} times in the list")
