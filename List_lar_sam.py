00f2bb79d5a59f60183181db929814dbfeaeaaa7
Madan-94c <madancmadanc498@gmail.com>
2025-11-14 12:32:22 +0530

    Create List_lar_sam.py

---
+++ List_lar_sam.py
@@ -0,0 +1,7 @@
+#3. Write a Python program to find the largest and smallest elements in a list.
+
+a = [10,20,30,40,60]
+largest = max(a)
+smallest = min(a)
+print("The largest element is:",largest)
+print("Thew smallest number is:",smallest)
