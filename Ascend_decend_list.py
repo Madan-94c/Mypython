7cd2a6eef03bc5719965b7a4f075ad2e69113ddd
Madan-94c <madancmadanc498@gmail.com>
2025-11-14 15:00:31 +0530

    Create Ascend_decend_list.py

---
+++ Ascend_decend_list.py
@@ -0,0 +1,6 @@
+#Write a Python program to sort a list in ascending order and descending order.
+a = [1,2,3]
+a.sort()
+print("ascending order:",a)
+a.sort(reverse=True)
+print("decending order:",a)
